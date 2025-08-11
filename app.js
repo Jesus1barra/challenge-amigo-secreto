9a7c64464fd195acc36ec2a128c182e1cc2b7dc1
Jesus1barra <jesusibarr@live.com.mx>
2025-08-11 12:46:27 -0600

    Crea el index.html

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log(ejecutando el sistema);
+//let textoIndicador = document.querySelector('h2');
+
+
+function agregarAmigo(){
+let listaDeNombres = document.getElementById('amigo').value;
+console.log (listaDeNombres);
+return
+}
+
+
+//Apartir de la segunda captura
+//textoIndicador.innerHTML = 'ingresa el nombre de tu nuevo amigo.'
